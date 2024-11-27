973e492ddf24b960512fa0fdf9b95b0674d2e0ca
jhoneber31 <jhoneber31@gmail.com>
2024-11-27 01:08:14 -0500

    [UPDATE] icon and title page

--- src/interface.ts
+++ src/interface.ts
@@ -1,14 +1,17 @@
+//interfaz de la respuesta de la api
 export interface ICripto {
   data:   Data;
   msg:    string;
   status: string;
 }
 
+//interfaz de la data de la respuesta de la api
 export interface Data {
   prediccion_adicional: PrediccionAdicional;
   resultados:           Array<number[]>;
 }
 
+//interfaz de la prediccion adicional de la respuesta de la api
 export interface PrediccionAdicional {
   fecha: string;
   valor: number;
